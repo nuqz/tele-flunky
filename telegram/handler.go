05fafe2d0010cfdfd619e645c142bf53ba39b1a8
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2018-04-04 22:37:29 +0300

    Return an error in handlers
    Signed-off-by: Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>

--- telegram/handler.go
+++ telegram/handler.go
@@ -1,9 +1,11 @@
 package telegram
 
-type HandlerFunc func(*Bot, *Update)
-
-func (fn HandlerFunc) HandleUpdate(bot *Bot, upd *Update) { fn(bot, upd) }
-
 type Handler interface {
-	HandleUpdate(*Bot, *Update)
+	HandleUpdate(*Bot, *Update) error
+}
+
+type HandlerFunc func(*Bot, *Update) error
+
+func (fn HandlerFunc) HandleUpdate(bot *Bot, upd *Update) error {
+	return fn(bot, upd)
 }
