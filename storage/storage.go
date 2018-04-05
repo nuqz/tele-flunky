cfc9538d8210fce12a185f9d3296e1b99e0a2228
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2018-04-05 23:30:59 +0300

    Access,Storage: add storage interfaces
    Signed-off-by: Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>

---
+++ storage/storage.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"gopkg.in/telegram-bot-api.v4"
+
+	"github.com/nuqz/tele-flunky/access"
+)
+
+type UserStorage interface {
+	PutUser(*access.User) error
+	HasUser(*tgbotapi.User) (bool, error)
+	GetUser(*tgbotapi.User)
+	DeleteUser(*access.User) error
+}
+
+type BotStorage interface {
+	UserStorage
+}
