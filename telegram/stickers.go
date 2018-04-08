9c8d2d93cac377dface53b597b10fcc36e70aab2
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2018-04-08 22:27:10 +0300

    Add some sticker IDs 4fun
    Signed-off-by: Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>

---
+++ telegram/stickers.go
@@ -0,0 +1,7 @@
+package telegram
+
+var (
+	StickerMinecraftFabulous     = "CAADAgADsQMAAvJ-ggzCPqB_xrc8iwI"
+	StickerMinecraftForeverAlone = "CAADAgADvAMAAvJ-ggyCesS6rGVfFgI"
+	StickerCriminalRaccoonHat    = "CAADAgADlwYAAvoLtgiTap3uNdUieAI"
+)
